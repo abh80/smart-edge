758674610187f23478068b48497e3fb931975632
abh80 <techixspotify@gmail.com>
2022-09-30 15:36:42 +0530

    refactor : Migrate to AccessibilityService

--- app/src/main/java/com/abh80/smartedge/MainActivity.java
+++ app/src/main/java/com/abh80/smartedge/MainActivity.java
@@ -1,19 +1,29 @@
 package com.abh80.smartedge;
 
 import android.Manifest;
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.pm.ServiceInfo;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.materialswitch.MaterialSwitch;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
@@ -23,22 +33,38 @@ public class MainActivity extends AppCompatActivity {
         init();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (!Settings.canDrawOverlays(this) || !Settings.Secure.getString(this.getContentResolver(), "enabled_notification_listeners").contains(getApplicationContext().getPackageName())
+        if (!isAccessibilityServiceEnabled(this, OverlayService.class) || !Settings.Secure.getString(this.getContentResolver(), "enabled_notification_listeners").contains(getApplicationContext().getPackageName())
                 || ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             startActivity(new Intent(this, PermissionActivity.class));
         }
-        MaterialSwitch enable_btn = findViewById(R.id.enable_switch);
+        MaterialCardView enable_btn = findViewById(R.id.enable_switch);
         enable_btn.setOnClickListener(l -> {
-            sharedPreferences.edit().putBoolean("enabled", enable_btn.isChecked()).apply();
+            Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(intent);
+            Toast.makeText(this, "Installed Apps -> Smart Edge", Toast.LENGTH_SHORT).show();
         });
-        enable_btn.setChecked(sharedPreferences.getBoolean("enabled", false));
         MaterialSwitch enable_btn2 = findViewById(R.id.enable_switch2);
         enable_btn2.setOnClickListener(l -> {
             sharedPreferences.edit().putBoolean("hwd_enabled", enable_btn2.isChecked()).apply();
         });
         enable_btn2.setChecked(sharedPreferences.getBoolean("hwd_enabled", false));
+        sharedPreferences.registerOnSharedPreferenceChangeListener((s, _v) -> {
+            sendBroadcast(new Intent(getPackageName() + ".SETTINGS_CHANGED"));
+        });
     }
 
+    public static boolean isAccessibilityServiceEnabled(Context context, Class<? extends AccessibilityService> service) {
+        AccessibilityManager am = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> enabledServices = am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
+
+        for (AccessibilityServiceInfo enabledService : enabledServices) {
+            ServiceInfo enabledServiceInfo = enabledService.getResolveInfo().serviceInfo;
+            if (enabledServiceInfo.packageName.equals(context.getPackageName()) && enabledServiceInfo.name.equals(service.getName()))
+                return true;
+        }
+
+        return false;
+    }
 
     private void init() {
         if (sharedPreferences == null) {
