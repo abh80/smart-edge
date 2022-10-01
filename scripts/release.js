1daa2943ebda83823d4f840aa1b0bd95b247ba53
abh80 <techixspotify@gmail.com>
2022-10-01 09:38:11 +0530

    workflow update

--- scripts/release.js
+++ scripts/release.js
@@ -17,7 +17,7 @@ const version = gradleBuild
   if (!TOKEN) return core.setFailed("No token was provided, exiting...");
   const octokit = github.getOctokit(TOKEN);
   const build_path =
-    process.cwd() + "/app/build/outputs/apk/release/app-release-unsigned.apk";
+    process.cwd() + "/app/build/outputs/apk/release/app-release.apk";
   const data = await octokit.rest.repos.createRelease({
     owner: "abh80",
     repo: "smart-edge",
@@ -25,7 +25,7 @@ const version = gradleBuild
     name: version,
     body: "🎉 " + version + " Released!",
   });
-  await octokit.rest.repos.uploadReleaseAsset({
+  const fdata = await octokit.rest.repos.uploadReleaseAsset({
     owner: "abh80",
     repo: "smart-edge",
     name: "release.apk",
@@ -33,4 +33,21 @@ const version = gradleBuild
     data: fs.readFileSync(build_path),
     release_id: data.data.id,
   });
+  const base_md = [
+    `# Smart Edge (Early Access) [![Build & Publish Debug APK](https://github.com/abh80/smart-edge/actions/workflows/release.yml/badge.svg)](https://github.com/abh80/smart-edge/actions/workflows/release.yml)
+  Alternative to dynamic island which shows your music status under the pinhole camera.`,
+    `# Previews
+
+  <img src = "https://user-images.githubusercontent.com/50198413/192252474-15852727-e487-4094-ae0f-bfc0f2c4ff06.png" width = "500"/>
+  
+  <img src = "https://user-images.githubusercontent.com/50198413/192252553-ee8fa52d-a3ec-4292-83a0-8ec3d9bb7787.png" width = "500"/>`,
+  ];
+  const downloadsmd = `# Downloads
+
+  [![Download Button](https://img.shields.io/github/v/release/abh80/smart-edge?color=7885FF&label=Android-Apk&logo=android&style=for-the-badge)](${fdata.data.browser_download_url})`;
+  fs.writeFileSync(
+    process.cwd() + "/README.md",
+    [base_md[0], downloadsmd, base_md[1]].join("\n")
+  );
+  core.setOutput("Successfully published apk!");
 })();
