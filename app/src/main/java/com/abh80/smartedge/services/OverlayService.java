f36dae0ce81896a571ee6b4077cf8891ad2db94e
abh80 <techixspotify@gmail.com>
2022-10-05 20:18:04 +0530

    refactor(OverlayService): use message instead of stack trace

--- app/src/main/java/com/abh80/smartedge/services/OverlayService.java
+++ app/src/main/java/com/abh80/smartedge/services/OverlayService.java
@@ -116,7 +116,7 @@ public class OverlayService extends AccessibilityService {
             if (sharedPreferences.getBoolean("clip_copy_enabled", true)) {
                 ClipboardManager clipboard = (ClipboardManager)
                         getSystemService(Context.CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("smart edge error log", Arrays.toString(throwable.getStackTrace()));
+                ClipData clip = ClipData.newPlainText("smart edge error log", throwable.getMessage());
                 clipboard.setPrimaryClip(clip);
                 Toast.makeText(this, "Smart Edge Crashed, logs copied to clipboard", Toast.LENGTH_SHORT).show();
             }
