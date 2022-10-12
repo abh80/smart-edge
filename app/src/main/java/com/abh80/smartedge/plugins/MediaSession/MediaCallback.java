3f25b62890385578870f04bf0b78f05ae8644c78
abh80 <techixspotify@gmail.com>
2022-10-12 15:36:30 +0530

    fix error reported in #33

--- app/src/main/java/com/abh80/smartedge/plugins/MediaSession/MediaCallback.java
+++ app/src/main/java/com/abh80/smartedge/plugins/MediaSession/MediaCallback.java
@@ -46,38 +46,43 @@ public class MediaCallback extends MediaController.Callback {
     @Override
     public void onPlaybackStateChanged(@Nullable PlaybackState state) {
         super.onPlaybackStateChanged(state);
-        if (state == null || mCurrent.getMetadata() == null) return;
-        MediaMetadata targetMetada = mCurrent.getMetadata();
-        boolean isPlaying2 = state.getState() == PlaybackState.STATE_PLAYING;
-        if (mediaMetadata != null && mediaMetadata.getString(MediaMetadata.METADATA_KEY_TITLE) != null
-                && mediaMetadata.getString(MediaMetadata.METADATA_KEY_TITLE).equals(targetMetada.getString(MediaMetadata.METADATA_KEY_TITLE)) && ctx.overlayOpen()) {
-            if (ctx.mCurrent != null && ctx.mCurrent.getPackageName().equals(mCurrent.getPackageName())) {
-                if (!isPlaying2) ctx.onPlayerPaused(true);
-                else ctx.onPlayerResume(true);
+        try {
+            if (state == null || mCurrent.getMetadata() == null) return;
+            MediaMetadata targetMetada = mCurrent.getMetadata();
+            boolean isPlaying2 = state.getState() == PlaybackState.STATE_PLAYING;
+            if (mediaMetadata != null && mediaMetadata.getString(MediaMetadata.METADATA_KEY_TITLE) != null
+                    && mediaMetadata.getString(MediaMetadata.METADATA_KEY_TITLE).equals(targetMetada.getString(MediaMetadata.METADATA_KEY_TITLE)) && ctx.overlayOpen()) {
+                if (ctx.mCurrent != null && ctx.mCurrent.getPackageName().equals(mCurrent.getPackageName())) {
+                    if (!isPlaying2) ctx.onPlayerPaused(true);
+                    else ctx.onPlayerResume(true);
+                }
+                isPlaying = isPlaying2;
+                return;
             }
-            isPlaying = isPlaying2;
-            return;
-        }
 
-        isPlaying = isPlaying2;
-        if (ctx.mCurrent != null && ctx.mCurrent.getPackageName().equals(mCurrent.getPackageName())) {
-            if (!isPlaying) ctx.onPlayerPaused(false);
-            else ctx.onPlayerResume(false);
-        }
-        if (!isPlaying) return;
-        mediaMetadata = targetMetada;
-        ctx.mCurrent = mCurrent;
-        if (ctx.expanded) {
-            updateView();
-            return;
-        }
-        ctx.closeOverlay(new CallBack() {
-            @Override
-            public void onFinish() {
-                super.onFinish();
-                updateView();
+            isPlaying = isPlaying2;
+            if (ctx.mCurrent != null && ctx.mCurrent.getPackageName().equals(mCurrent.getPackageName())) {
+                if (!isPlaying) ctx.onPlayerPaused(false);
+                else ctx.onPlayerResume(false);
             }
-        });
+            if (!isPlaying) return;
+            mediaMetadata = targetMetada;
+            ctx.mCurrent = mCurrent;
+            if (ctx.expanded) {
+                updateView();
+                return;
+            }
+            ctx.closeOverlay(new CallBack() {
+                @Override
+                public void onFinish() {
+                    super.onFinish();
+                    updateView();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            ctx.closeOverlay();
+        }
     }
 
     @Override
